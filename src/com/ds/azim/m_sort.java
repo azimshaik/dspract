6a89be96357f901cbd7c2d95071149c0b2803c81
azimshaik <cheer.buddu@gmail.com>
2016-12-30 11:23:21 -0500

    Hell yeahgit add * it Works

--- src/com/ds/azim/m_sort.java
+++ src/com/ds/azim/m_sort.java
@@ -1,14 +1,15 @@
 package com.ds.azim;
 
 public class m_sort {
-	public static void merge_sort(int[] a){
+	
+	public static void merg_helper(int[] a){
 		merge_sort(a,0,a.length-1);
 	}
 	public static void merge_sort(int[] a, int low, int high){
 		if(low>=high){
 			return;
 		}else{
-			int mid = (low+high)/2;
+			int mid = low/2 + high/2;
 			merge_sort(a,low,mid);
 			merge_sort(a,mid+1,high);
 			merge(a,low,mid,high);
@@ -18,7 +19,7 @@ public class m_sort {
 		int[] temp = new int[high-low+1];
 		int left = low;
 		int right = mid+1;
-		int k = 0;
+		int k = 0; //index for the temp array
 		while(left<=mid && right<=high){
 			if(a[left]<a[right]){
 				temp[k] = a[left];
@@ -37,22 +38,20 @@ public class m_sort {
 			}
 		}else{
 			while(right<=high){
-				temp[k]=a[right];
+				temp[k] = a[right];
 				right++;
 				k++;
 			}
 		}
-		for (int i = 0; i<temp.length;i++){
+		for(int i=0 ;i<temp.length;i++){
 			a[low+i] = temp[i];
 		}
-		
 	}
 	public static void main(String[] args){
-		int[] a = {89,67,-34,0,-87,-987};
-		merge_sort(a);
-		for (int i = 0; i<a.length;i++){
-			System.out.print(a[i]+",");
+		int[] a = {23,4,76,-7,-4};
+		merg_helper(a);
+		for(int m=0 ; m<a.length; m++){
+			System.out.print(a[m]+",");
 		}
-		
 	}
 }
