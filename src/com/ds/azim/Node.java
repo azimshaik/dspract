ae33dda889eed3d8a4bcfd0f98cef131723494fd
azimshaik <cheer.buddu@gmail.com>
2016-12-30 10:43:26 -0500

    SLL Insert Last and End Working

--- src/com/ds/azim/Node.java
+++ src/com/ds/azim/Node.java
@@ -1,6 +1,18 @@
 package com.ds.azim;
 
 public class Node {
-	
+	public int data;
+	public Node next;
+	public Node(){
+		
+	}
+	public Node(int data){
+		this.data = data;
+		this.next = null;
+	}
+	public Node(int data, Node next){
+		this.data = data;
+		this.next = next;
+	}
 
 }
--- src/com/ds/azim/SingleLinkedList.java
+++ src/com/ds/azim/SingleLinkedList.java
@@ -1,5 +1,40 @@
 package com.ds.azim;
 
 public class SingleLinkedList {
-	
+	public Node head = null;
+	public Node tail = null;
+	public SingleLinkedList(){
+		
+	}
+	public void insertAtFirst(int data){
+		head = new Node(data,head);
+	}
+	public void insertAtEnd(int data){
+		if(head==null){
+			insertAtFirst(data);
+		}else{
+			Node curr = head;
+			while(curr.next!=null){
+				curr = curr.next;
+			}
+			curr.next = new Node(data);
+		}
+	}
+	public void show(){
+		Node curr = head;
+		while(curr!=null){
+			System.out.print(curr.data+",");
+			curr = curr.next;
+		}
+	}
+	public static void main(String[] args){
+		SingleLinkedList sll = new SingleLinkedList();
+		sll.insertAtFirst(1223);
+		sll.insertAtFirst(456);
+		sll.insertAtFirst(-89);
+//		sll.show();
+		sll.insertAtEnd(46);
+		sll.insertAtEnd(34);
+		sll.show();
+	}
 }
